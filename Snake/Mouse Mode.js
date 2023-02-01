d78c8511ce1df47343fdd6deb46690a5f178db0b
Boat <118922095+Believaboat@users.noreply.github.com>
2023-02-01 17:52:33 -0500

    Update Mouse Mode.js

--- Snake/Mouse Mode.js
+++ Snake/Mouse Mode.js
@@ -1,6 +1,1 @@
-req = new XMLHttpRequest();
-req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeMouseMode/main/mouse.js');
-req.onload = function() {
-	(1, eval)(this.responseText);
-};
-req.send();
+javascript:req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeMouseMode/main/mouse.js'); req.onload = function() { (1, eval)(this.responseText); }; req.send();
