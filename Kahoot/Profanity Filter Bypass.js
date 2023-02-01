34f332263173b4e293fb434c260de09058fa2943
Boat <118922095+Believaboat@users.noreply.github.com>
2023-02-01 17:50:33 -0500

    Bookmarkletify

--- Kahoot/Profanity Filter Bypass.js
+++ Kahoot/Profanity Filter Bypass.js
@@ -1,5 +1,1 @@
-fetch('https://thatfrueddued.github.io/kahoot-smasher/BadNameFilterBypass.js').then(function(response) {
-	response.text().then(function(text) {
-		eval(text);
-	});
-});
+javascript:fetch('https://thatfrueddued.github.io/kahoot-smasher/BadNameFilterBypass.js').then(function(response) { response.text().then(function(text) { eval(text); }); });
