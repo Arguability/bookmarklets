2c9e22d1e3cf4d09396fb44dabee9a2b0115aaa8
Boat <118922095+Believaboat@users.noreply.github.com>
2023-02-01 17:52:08 -0500

    Bookmarkletify

--- Popcat Clicker/Popcat Autoclicker.js
+++ Popcat Clicker/Popcat Autoclicker.js
@@ -1,10 +1,1 @@
-var event = new KeyboardEvent('keydown', {
-	key: 'g',
-	ctrlKey: true
-});
-
-setInterval(function(){
-	for (i = 0; i < 100; i++) {
-		document.dispatchEvent(event);
-	}
-}, 0);
+javascript:var event = new KeyboardEvent('keydown', { key: 'g', ctrlKey: true }); setInterval(function(){ for (i = 0; i < 100; i++) { document.dispatchEvent(event); } }, 0);
