91c7992fb4a9f62ff5d5fca3786280493742123e
Boat <118922095+Believaboat@users.noreply.github.com>
2023-02-01 17:52:22 -0500

    Bookmarkletify

--- Snake/MoreMenu.js
+++ Snake/MoreMenu.js
@@ -1,6 +1,1 @@
-req = new XMLHttpRequest();
-req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeCustomMenuStuff/main/custom.js');
-req.onload = function() {
-	eval(this.responseText + 'snake.more_menu();');
-};
-req.send();
+javascript:req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeCustomMenuStuff/main/custom.js'); req.onload = function() { eval(this.responseText + 'snake.more_menu();'); }; req.send();
